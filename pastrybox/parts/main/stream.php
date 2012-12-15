cacd9ad90c425664b781926a5634c14bc92ab1e8
Barnaby Walters <barnaby@waterpigs.co.uk>
2012-12-15 17:45:33 +0000

    Added microformats-2 to markup

--- pastrybox/parts/main/stream.php
+++ pastrybox/parts/main/stream.php
@@ -10,23 +10,23 @@
     $theBaker = $bakers->get_by_slug( $category[0]->slug );
     ?>
 		        
-    <div class="thought thought-format-b step" role="article">
+    <div class="thought thought-format-b step h-entry hentry" role="article">
         <h3 class="thought-title">
-            <a href="<?php pastry_the_thought_permalink( $category[0], $theThought ); ?>"><?php pastry_the_thought_title_to_date( $theThought->post_title ); ?></a>
+            <a class="p-name u-url" href="<?php pastry_the_thought_permalink( $category[0], $theThought ); ?>"><?php pastry_the_thought_title_to_date( $theThought->post_title ); ?></a>
         </h3>
 		      
         <div class="row">
-            <div class="baker-info span3 pull-left">		                      
+            <div class="baker-info span3 pull-left p-author author h-card vcard">		                      
                 <h4 class="baked-by a-area-b">
-                    <span><a href="<?php echo $theBaker->profile_url; ?>"><?php echo $theBaker->name; ?></a></span>
+                    <span><a class="p-name fn u-url url" href="<?php echo $theBaker->profile_url; ?>"><?php echo $theBaker->name; ?></a></span>
                 </h4>					
                 <p class="baker-portrait">
-                    <img src="<?php echo $theBaker->portrait_small_url; ?>" alt="<?php echo $theBaker->portrait_alt; ?>" class="portrait-small">
+                    <img class="u-photo photo" src="<?php echo $theBaker->portrait_small_url; ?>" alt="<?php echo $theBaker->portrait_alt; ?>" class="portrait-small">
                 </p>
             </div><!-- /.baker-info -->
             
             <div class="span9 pull-right">
-                <blockquote class="thought-content a-area-a">  
+                <blockquote class="thought-content a-area-a e-content entry-content">  
                     <?php echo pastry_clean_code( $theThought->post_content ); ?>                   
                 </blockquote>
             </div><!-- /.span9 -->	
